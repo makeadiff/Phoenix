e87ccd9b450b02786644cf536ef4425ebcbc61cb
Binny V A <binnyva@gmail.com>
2019-05-11 18:17:15 +0530

    Better way to get project/group IDs, config files added

---
+++ app/Models/Credit.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Models;
+
+use App\Models\Common;
+// NOT USED YET. Sample code
+final class Credit extends Common  
+{
+    protected $table = 'UserCredit';
+    public $timestamps = false;
+
+    protected $rules = [
+        'ed'    => [
+            'class' => [
+                'miss_zero_hour'    => -0.5,
+                'substitute'        => 1,
+                'absent_with_substitute'    => -1,
+                'absent_without_substitute' => -2
+            ],
+            'event' => [
+                // 13 is 'event_type_id' - also, 13 is a sample. Not actual.
+                13 => [
+                    'absent_rsvped_no'  => -0.5,
+                    'absent_rsvped_yes' => -1,
+                    'present'           => 1
+                ]
+            ]
+        ],
+        /*
+        'ac'    => [
+            'event' => [
+                aftercare_circle
+            ]
+        ]
+         */
+        // 'fr'    => [
+        //     'event' => [
+        //     ]
+        // ]
+    ];
+
+    public function calculate($situation, $user_id, $situation_id)
+    {
+        // $situation - event,class
+        // $situation_id - Class.id, Event.id
+
+    }
+
+}
+
---
+++ config/constants.php
@@ -0,0 +1,75 @@
+<?php 
+return [
+	'project' => [
+		'ed'	=> [
+			'id'	=> 1,
+			'name'	=> 'Ed Support'
+		],
+		'fp'	=> [
+			'id'	=> 2,
+			'name'	=> 'Fonudational Programme'
+		],
+		'tr_asv'	=> [
+			'id'	=> 4,
+			'name'	=> 'Transition Readiness - ASV'
+		],
+		'tr_wingman'	=> [
+			'id'	=> 5,
+			'name'	=> 'Transition Readiness - Wingman'
+		],
+		'ac'	=> [
+			'id'	=> 6,
+			'name'	=> 'Aftercare'
+		],
+	],
+	'group'	=> [
+		'ed'	=> [
+			'teacher' => [
+				'id'	=> 9,
+				'name'	=> 'ES Volunteer'
+			],
+			'mentor' => [
+				'id'	=> 8,
+				'name'	=> 'ES Mentor'
+			],
+			'trained' => [
+				'id'	=> 368,
+				'name'	=> 'ES Trained'
+			],
+		],
+		'fp'	=> [
+			'teacher' => [
+				'id'	=> 376,
+				'name'	=> 'Foundational Skills Volunteer'
+			],
+			'mentor' => [
+				'id'	=> 386,
+				'name'	=> 'Foundation Mentor'
+			],
+			'trained' => [
+				'id'	=> 387,
+				'name'	=> 'Foundation Trained'
+			],
+		],
+		'ac'	=> [
+			'asv'	=> [
+				'id'	=> 377,
+				'name'	=> 'Aftercare ASV'
+			],
+			'wingman'	=> [
+				'id'	=> 365,
+				'name'	=> 'Aftercare Wingman'
+			],
+		],
+		'tr'	=> [
+			'asv'	=> [
+				'id'	=> 349,
+				'name'	=> 'Transition Readiness ASV'
+			],
+			'wingman'	=> [
+				'id'	=> 348,
+				'name'	=> 'Transition Readiness Wingman'
+			],
+		]
+	]
+];
