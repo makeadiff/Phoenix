9469e0ab68c7a50404fdf4a01c8ae46742b7379e
Binny V A <binnyva@gmail.com>
2019-10-15 21:23:37 +0530

    Added a few unit tests. Need to use this more. Checks the return of the model methord itself - rather than looking at the api call response

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -4,9 +4,14 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 
+/**
+ * @runInSeparateProcess
+ */
 class ExampleTest extends TestCase
 {
+	use WithoutMiddleware;
     /**
      * A basic test example.
      *
