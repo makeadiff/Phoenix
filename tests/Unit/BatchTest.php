9f2db6d4ee34f8f186dbb773f506fad798aa5458
Binny V A <binnyva@gmail.com>
2019-10-15 23:31:16 +0530

    Added a lot of unit tests. And a generator that creates the Unit tests

--- tests/Unit/BatchTest.php
+++ tests/Unit/BatchTest.php
@@ -35,8 +35,35 @@ class BatchTest extends TestCase
         $this->assertSame($result->day, '0');
     }
 
-    // :TODO: teacher_id, direction, project_id
+    public function testBatchSearchWithTeacher()
+    {
+        $batch = new Batch;
+        $batches = $batch->search(['teacher_id' => 1]);
 
+        $result = $batches->first();
 
+        $this->assertSame($result->id, 2652);
+        $this->assertSame($result->day, '0');
+    }
 
+    public function testBatchSearchWithProject()
+    {
+        $batch = new Batch;
+        $batches = $batch->search(['project_id' => 2, 'center_id' => 240]);
+
+        $result = $batches->first();
+
+        $this->assertSame($result->id, 2980);
+        $this->assertSame($result->day, '0');
+    }
+
+    public function testBatchName()
+    {
+        $batch = new Batch;
+        $name = $batch->find(2609)->name();
+
+        $this->assertEquals($name, 'Sunday 12:00 AM');
+    }
+
+    // :TODO: direction
 }
