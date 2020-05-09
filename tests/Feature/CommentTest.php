7f0822e06a20b1d25e008ff9c893f8dffee7ba8f
Binny V A <binnyva@gmail.com>
2020-05-09 23:19:55 +0530

    Wrote tests for Comments - Create, List and delete. Students and Center.

--- tests/Feature/CommentTest.php
+++ tests/Feature/CommentTest.php
@@ -15,6 +15,103 @@ class CommentTest extends TestCase
 
     // protected $only_priority_tests = true;
     // protected $write_to_db = false;
+    protected $student_id = 21927; // Han in Test City
+
+    // Path: GET /centers/{center_id}/comments
+    public function testCenterComments()
+    {
+        if ($this->only_priority_tests) {
+            $this->markTestSkipped("Running only priority tests.");
+        }
+
+        $center_id = 25;
+
+        $this->load("/centers/$center_id/comments", 'GET');
+
+        $db_comment_ids = app('db')->table('Comment')->select('id', 'comment')
+            ->where('item_type', 'Center')->where('item_id', $center_id)
+            ->pluck('id')->toArray();
+
+        $found = 0;
+        foreach ($this->response_data->data->comments as $l) {
+            if (in_array($l->id, $db_comment_ids)) {
+                $found ++;
+            }
+        }
+        $this->assertEquals($found, count($db_comment_ids));
+    }
+
+    /// Path: POST    /students/{student_id}/comments
+    public function testCreateStudentComment()
+    {
+        if ($this->only_priority_tests) {
+            $this->markTestSkipped("Running only priority tests.");
+        }
+        if (!$this->write_to_db) {
+            $this->markTestSkipped("Skipping as this test writes to the Database.");
+        }
+
+        $this->load("/students/{$this->student_id}/comments", 'POST', [
+            'comment'   => "Hello World",
+            "added_by_user_id"  => 1
+        ]);
+
+        $this->assertEquals($this->response_data->status, 'success');
+        $created_comment_id = $this->response_data->data->comment->id;
+        $this->assertEquals($this->response->getStatusCode(), 200);
+
+        $db_comments = app('db')->table('Comment')->select('id', 'comment')
+            ->where('item_type', 'Student')->where('item_id', $this->student_id)
+            ->pluck('comment')->toArray();
+        $this->assertTrue(in_array('Hello World', $db_comments));
+
+        return $created_comment_id;
+    }
+
+    // Path: GET /students/{student_id}/comments
+    /**
+     * @depends testCreateStudentComment
+     */
+    public function testStudentComments($created_comment_id)
+    {
+        if ($this->only_priority_tests) {
+            $this->markTestSkipped("Running only priority tests.");
+        }
+
+        $this->load("/students/{$this->student_id}/comments", 'GET');
+
+        $found = false;
+        foreach ($this->response_data->data->comments as $l) {
+            if ($l->comment == "Hello World") {
+                $found = true;
+            }
+        }
+        $this->assertTrue($found);
+    }
+
+    /// Path: DELETE    /students/{student_id}/comments/{comment_id}
+    /**
+     * @depends testCreateStudentComment
+     */
+    public function testDeletecomment($created_comment_id)
+    {
+        if ($this->only_priority_tests) {
+            $this->markTestSkipped("Running only priority tests.");
+        }
+        if (!$this->write_to_db) {
+            $this->markTestSkipped("Skipping as this test writes to the Database.");
+        }
+        if (!$created_comment_id) {
+            $this->markTestSkipped("Can't find ID of comment created as test.");
+        }
+
+        $this->load("/students/{$this->student_id}/comments/{$created_comment_id}", 'DELETE');
+        $this->assertEquals($this->response->getStatusCode(), 200);
+
+        $found = app('db')->table("Comment")->where('id', $created_comment_id)->count();
+        $this->assertEquals($found, '0'); // Its actually deleted.
+    }
+
 
     // GraphQL: {center(id: ID) { id comments { comment }}}
     public function testCenterComment()
