591a16bab68d6ea603aeb460a34af6ee1a177469
Binny V A <binnyva@gmail.com>
2020-07-08 22:19:00 +0530

    Fixed the tests. Parameter IDs updated

--- tests/Unit/EventTest.php
+++ tests/Unit/EventTest.php
@@ -25,7 +25,7 @@ class EventTest extends TestCase
         $event_model = new Event;
         $event_model->updateUserConnection($this->user_id, ['present' => 3, 'rsvp' => 'no_data'], $this->event_id);
 
-        $para_credit_lost_by_missing_ac_circle_without_informing = 18;
+        $para_credit_lost_by_missing_ac_circle_without_informing = 11;
         $credit_model = new Credit;
         $credit_data = $credit_model->search([
                 'user_id' => $this->user_id, 
@@ -33,7 +33,7 @@ class EventTest extends TestCase
                 'item' => 'Event', 
                 'item_id' => $this->event_id
             ]);
-        $this->assertEquals($credit_data[0]->change, -2);
+        $this->assertEquals($credit_data[count($credit_data) - 1]->change, -2);
 
         $this->assertEquals($attender_credit - 2, $user_model->find($this->user_id)->credit); // For teacher
     }
@@ -46,7 +46,7 @@ class EventTest extends TestCase
         $event_model = new Event;
         $event_model->updateUserConnection($this->user_id, ['present' => 3, 'rsvp' => 'cant_go'], $this->event_id);
 
-        $para_credit_lost_by_missing_ac_circle_after_informing = 17;
+        $para_credit_lost_by_missing_ac_circle_after_informing = 10;
         $credit_model = new Credit;
         $credit_data = $credit_model->search([
                 'user_id' => $this->user_id, 
@@ -54,7 +54,7 @@ class EventTest extends TestCase
                 'item' => 'Event', 
                 'item_id' => $this->event_id
             ]);
-        $this->assertEquals($credit_data[0]->change, -1);
+        $this->assertEquals($credit_data[count($credit_data) - 1]->change, -1);
 
         $this->assertEquals($attender_credit + 1, $user_model->find($this->user_id)->credit); // Yes - the credit goes UP
     }
