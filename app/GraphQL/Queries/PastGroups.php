6a6311d786dded117d971f6bfc12f5370602b7d1
Bhanu Gupta <bhanu.mbvg@gmail.com>
2021-07-13 20:43:40 +0530

    feat(add): graphql schema and testing features

---
+++ app/GraphQL/Queries/PastGroups.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\GraphQL\Queries;
+
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+use App\Models\User;
+
+class PastGroups
+{
+    /**
+     * Return a value for the field.
+     *
+     * @param  null  $rootValue Usually contains the result returned from the parent field. In this case, it is always `null`.
+     * @param  mixed[]  $args The arguments that were passed into the field.
+     * @param  \Nuwave\Lighthouse\Support\Contracts\GraphQLContext  $context Arbitrary data that is shared between all fields of a single query.
+     * @param  \GraphQL\Type\Definition\ResolveInfo  $resolveInfo Information about the query itself, such as the execution state, the field name, path to the field from the root, and more.
+     * @return mixed
+     */
+    public function resolve($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    {
+        if (isset($args['user_id'])){
+            $user_id = $args['user_id'];
+            $user = new User;
+        } else{
+            return false;
+        }
+
+        $past_groups = $user->fetch($user_id)->pastGroups()->get();
+        
+        return $past_groups;
+    }
+}
