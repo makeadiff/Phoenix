210b5ffcf1c2871c712e9f60cede3ad571285984
Binny V A <binnyva@gmail.com>
2021-10-28 15:56:33 +0530

    Seeds the student DB after the change

--- database/migrations/2021_10_16_093148_student_add_status.php
+++ database/migrations/2021_10_16_093148_student_add_status.php
@@ -21,8 +21,8 @@ class StudentAddStatus extends Migration
         Schema::table('Student', function (Blueprint $table) {
             $table->enum('student_type', ['active', 'active_away', 'alumni', 'alumni_no_contact', 'alumni_dead', 'other'])
                         ->nullable()->default(null)->after('reason_for_leaving');
-            // DB::statement("ALTER TABLE `Conversation` CHANGE `type` `type` SET('appreciation','check-in','developmental','exit') NOT NULL DEFAULT 'check-in';");
         });
+        DB::statement("UPDATE Student SET student_type = 'active' WHERE center_id IN (SELECT id FROM Center WHERE status = '1' AND city_id != 14) AND status = '1'");
     }
 
     /**
